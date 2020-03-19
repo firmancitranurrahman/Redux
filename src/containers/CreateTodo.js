3273af823d4cafbd5d7166539389c286a42c7114
firmancitranurrahman <firmancitranurrahman@gmail.com>
2020-03-19 22:04:39 +0700

    pindahan

---
+++ src/containers/CreateTodo.js
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import {addTodo} from '../action/actionCreator';
+import {bindActionCreators} from 'redux';
+
+class CreateTodo extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            todotext: '',
+        }
+        this.onChangeTodoText = this.onChangeTodoText.bind(this);
+    }
+
+    onChangeTodoText(e){
+        this.setState({
+            todotext: e.target.value
+        })
+    }
+
+    render(){
+        return(
+            <div className="form-group row">
+                <div className="col-sm-10">
+                    <input type="text" className="form-control" name="" id="" onChange={this.onChangeTodoText} value={this.state.todotext} placeholder="add todo here"  />
+                    <button type="submit" onClick={ () => this.setState({todotext:''})} style={{marginTop:"25px",marginRight:"15px"}} className="btn btn-danger">Cancel</button>
+                    <button type="submit" onClick={ () => { this.props.addTodo(this.state.todotext); this.setState({todotext: ''})}} className="btn btn-success" style={{marginTop:"25px"}}>Add Todo</button>
+                </div>
+            </div>
+        )
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        addTodo
+    }, dispatch)
+}
+export default connect(null, mapDispatchToProps)(CreateTodo)
